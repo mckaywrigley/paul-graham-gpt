b6bcbf54e605708ad3c4c0d1e1e139552f4decd2
Mckay Wrigley <mckaywrigley@gmail.com>
2023-03-04 09:13:16 -0700

    add openai link

--- pages/index.tsx
+++ pages/index.tsx
@@ -308,7 +308,16 @@ export default function Home() {
                 </button>
               </div>
             ) : (
-              <div className="text-center font-bold text-3xl mt-7">Please enter your OpenAI API key in settings.</div>
+              <div className="text-center font-bold text-3xl mt-7">
+                Please enter your
+                <a
+                  className="mx-2 underline hover:opacity-50"
+                  href="https://openai.com/product"
+                >
+                  OpenAI API key
+                </a>
+                in settings.
+              </div>
             )}
 
             {loading ? (
